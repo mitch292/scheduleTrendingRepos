0ae5de13c4f54f84fae56f4d51c2c933f60b78d8
Andrew Mitchell <andrewpmitchell7@gmail.com>
2021-10-21 14:16:41 -0400

    Adds some logging to cloudflare kv storage

--- src/handler.ts
+++ src/handler.ts
@@ -1,11 +1,20 @@
 import { PROCESS_REPOS_URL } from './config'
 export const triggerEvent = async (): Promise<void> => {
-  await fetch(PROCESS_REPOS_URL, {
-    headers: {
-      'Content-Type': 'application/json',
+  try {
+    const res = await fetch(PROCESS_REPOS_URL, {
+      headers: {
+        'Content-Type': 'application/json',
+        // eslint-disable-next-line @typescript-eslint/ban-ts-comment 
+        // @ts-ignore This value is available via cloudflare
+        Authorization: `Bearer ${API_TOKEN}`,
+      },
+    });
       // eslint-disable-next-line @typescript-eslint/ban-ts-comment 
       // @ts-ignore This value is available via cloudflare
-      Authorization: `Bearer ${API_TOKEN}`,
-    },
-  })
+      await TRENDING_REPOS_FETCH.put(`${(new Date).toISOString()}::res`, res.status)
+  } catch (error) {
+      // eslint-disable-next-line @typescript-eslint/ban-ts-comment 
+      // @ts-ignore This value is available via cloudflare
+      await TRENDING_REPOS_FETCH.put(`${(new Date).toISOString()}::err`, `${error.status}: ${error.message}`)
+  }
 }
